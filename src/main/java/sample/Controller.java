57eec2f436381aaea02d81e3eeb5e290a9d70c98
Rm Raja <r.m_raja@yahoo.com>
2020-06-13 18:14:28 +0800

    Completed Basic arithmetic logic and support for decimal inputs.

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,90 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    private String x, y, operand, result = null;
+
+    @FXML
+    private JFXTextField resultText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void handleNumberClickEvent(ActionEvent event) {
+        JFXButton button = (JFXButton) event.getSource();
+        String input = button.getText();
+        if (this.operand == null) {
+            if (this.x == null) {
+                this.x = input;
+            } else {
+                this.x += input;
+            }
+            resultText.setText(this.x);
+        } else {
+            if (this.y == null) {
+                this.y = input;
+            } else {
+                this.y += input;
+            }
+            resultText.setText(this.y);
+        }
+    }
+
+    @FXML
+    private void handleActionClickEvent(ActionEvent event) {
+        JFXButton button = (JFXButton) event.getSource();
+        if (this.operand != null) {
+            if (this.x != null && this.y != null) {
+                String calcResult = calculate();
+                resultText.setText(calcResult);
+                this.x = calcResult;
+                this.y = null;
+            }
+        }
+        this.operand = button.getText();
+    }
+
+    private String calculate() {
+        double a = Double.parseDouble(this.x);
+        double b = Double.parseDouble(this.y);
+        double output = 0.00;
+        switch (this.operand) {
+            case "+":
+                output = a + b;
+                break;
+            case "-":
+                output = a - b;
+                break;
+            case "/":
+                output = a / b;
+                break;
+            case "*":
+                output = a * b;
+                break;
+            default:
+                break;
+        }
+        return new DecimalFormat("#.##").format(output);
+    }
+
+    @FXML
+    private void resetCalculator() {
+        this.x = null;
+        this.y = null;
+        this.operand = null;
+        this.resultText.setText("0.00");
+    }
 }
